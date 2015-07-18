9b4935fecfa72a4379827e889c4d18c44579b7c3
-- C4 -- <info@c4ios.com>
2015-07-17 22:36:53 -0700

    Adds code for handshaking with peripheral

--- MOCore/AppDelegate.swift
+++ MOCore/AppDelegate.swift
@@ -16,9 +16,10 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSNetServiceDelegate, GCDAsy
     //The core's main socket, all peripherals will connect to this one
     var asyncSocket : GCDAsyncSocket?
 
-    //A list of sockets that keeps track of peripherals that have successfully connected
+    //A list of all the sockets that have been connected
     var connectedSockets = [GCDAsyncSocket]()
-    
+
+
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         //creates the primary socket, on the main queue
         asyncSocket = GCDAsyncSocket(delegate: self, delegateQueue: dispatch_get_main_queue())
@@ -42,6 +43,34 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSNetServiceDelegate, GCDAsy
             //couldn't set up the socket to accept connections
             println("Error in acceptOnPort:error: -> \(error)")
         }
+    }
+
+    func socket(sock: GCDAsyncSocket!, didAcceptNewSocket newSocket: GCDAsyncSocket!) {
+        println("\(__FUNCTION__) from: \(newSocket.connectedHost):\(newSocket.connectedPort)")
+        connectedSockets.append(newSocket)
+        writeTo(newSocket, message: "handshake-from-central")
+    }
+
+    func writeTo(sock: GCDAsyncSocket, message: String) {
+        println(__FUNCTION__)
+        //converts the message to data
+        let data = NSMutableData(data: message.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!)
+        //appends an extra bit of data that acts as an "end point" for reading
+        data.appendData(GCDAsyncSocket.CRLFData())
+        //writes the full data to the socket
+        sock.writeData(data, withTimeout: -1, tag: 0)
+        //tells the socket to read until it reaches the "end point"
+        sock.readDataToData(GCDAsyncSocket.CRLFData(), withTimeout: -1, tag: 0)
+    }
+
+    func socket(sock: GCDAsyncSocket!, didWriteDataWithTag tag: Int) {
+        println(__FUNCTION__)
+        sock.readDataWithTimeout(-1, tag: 0)
+    }
+
+    func socket(sock: GCDAsyncSocket!, didReadData data: NSData!, withTag tag: Int) {
+        let s = NSString(data: data, encoding: NSUTF8StringEncoding)
+        println("\(__FUNCTION__) message: \(s)")
     }
 }
 
