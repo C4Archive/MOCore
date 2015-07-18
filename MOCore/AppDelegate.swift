be3a79abd3938c0e4b925121ed07272969e48ffd
travis <travis@c4ios.com>
2015-07-17 17:15:14 -0700

    Adds GCDASyncSocket
    Includes basic app delegate code for setting up a socket and publishing
    a net service.

--- MOCore/AppDelegate.swift
+++ MOCore/AppDelegate.swift
@@ -9,18 +9,35 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+class AppDelegate: NSObject, NSApplicationDelegate, NSNetServiceDelegate, GCDAsyncSocketDelegate  {
+    //The service used to broadcast the core, peripherals will find this and connect to it
+    var netService : NSNetService?
+    //The core's main socket, all peripherals will connect to this one
+    var asyncSocket : GCDAsyncSocket?
+    //A list of sockets that keeps track of peripherals that have successfully connected
+    var connectedSockets = [GCDAsyncSocket]()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        //creates the primary socket, on the main queue
+        asyncSocket = GCDAsyncSocket(delegate: self, delegateQueue: dispatch_get_main_queue())
+        
+        //tells the socket to begin listening and accepting connections
+        var error : NSError?
+        if asyncSocket?.acceptOnPort(0, error: &error) == true {
+            //if the socket is able to accept, and has a valid local port number
+            if let port : UInt16 = asyncSocket?.localPort {
+                //then set up the service to be published
+                netService = NSNetService(domain: "local.", type: "_m-o._tcp.", name: "m-o-centralService", port: Int32(port))
+                netService?.delegate = self
+                netService?.publish()
+            }
+        } else {
+            //couldn't set up the socket to accept connections
+            println("Error in acceptOnPort:error: -> \(error)")
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
     }
-
-
 }
 
