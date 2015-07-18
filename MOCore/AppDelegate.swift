a2d3402deb318bb7f58d02d200efc23072bf4793
-- C4 -- <info@c4ios.com>
2015-07-17 20:12:57 -0700

    update service name

--- MOCore/AppDelegate.swift
+++ MOCore/AppDelegate.swift
@@ -12,8 +12,10 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate, NSNetServiceDelegate, GCDAsyncSocketDelegate  {
     //The service used to broadcast the core, peripherals will find this and connect to it
     var netService : NSNetService?
+
     //The core's main socket, all peripherals will connect to this one
     var asyncSocket : GCDAsyncSocket?
+
     //A list of sockets that keeps track of peripherals that have successfully connected
     var connectedSockets = [GCDAsyncSocket]()
     
@@ -24,20 +26,22 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSNetServiceDelegate, GCDAsy
         //tells the socket to begin listening and accepting connections
         var error : NSError?
         if asyncSocket?.acceptOnPort(0, error: &error) == true {
+
             //if the socket is able to accept, and has a valid local port number
             if let port : UInt16 = asyncSocket?.localPort {
                 //then set up the service to be published
-                netService = NSNetService(domain: "local.", type: "_m-o._tcp.", name: "m-o-centralService", port: Int32(port))
+                netService = NSNetService(domain: "local.", type: "_m-o._tcp.", name: "m-o-core-service", port: Int32(port))
+
+                //set the net service's delegate
                 netService?.delegate = self
+
+                //publish the service
                 netService?.publish()
             }
         } else {
             //couldn't set up the socket to accept connections
             println("Error in acceptOnPort:error: -> \(error)")
         }
-    }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
     }
 }
 
